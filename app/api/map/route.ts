d5066befc026b725b14b3bc2ac666fa74608a2b9
tolgakavukcu <kavukcu.tolga@gmail.com>
2023-06-27 19:36:21 +0200

    map fix

--- app/api/map/route.ts
+++ app/api/map/route.ts
@@ -4,6 +4,11 @@ export async function GET(req: NextRequest) {
   
     const lat = req.geo?.latitude
     const long = req.geo?.longitude;
+
+    console.log(JSON.stringify(req.geo))
+    console.log("api lat "  + req.headers.get("x-vercel-ip-latitude"));
+    console.log("api long "  +  req.headers.get("x-vercel-ip-longitude"));
+    console.log("api city "  +  req.headers.get("x-vercel-ip-city"));
   
     return NextResponse.json({lat, long});
   }
