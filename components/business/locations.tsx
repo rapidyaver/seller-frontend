ef2d82f9fb85bb57aa515458af301742ff2c360e
tolgakavukcu <kavukcu.tolga@gmail.com>
2023-06-29 21:56:09 +0200

    more mobile fixes

--- components/business/locations.tsx
+++ components/business/locations.tsx
@@ -14,31 +14,34 @@ export default function BusinessLocations({
       <ul role="list" className="mt-1">
         {
           locations.map((location, key ) => {
-            return (<li className="group flex justify-between gap-x-6 py-5 my-2 border-2 p-2 rounded-md hover:cursor-pointer" key={location.id}>
-            <div className="flex gap-x-4 items-center group-hover:opacity-75">
-              <Image
-                width={256} height={256}
-                className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                src={location.imageUrl}
-                alt=""
-              ></Image>
-              <div className="min-w-0 flex-auto max-w-md">
-                <p className="text-sm font-semibold leading-6 text-gray-900">
-                  {location.name}
-                </p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                  {location.address}
+            return (
+            <li className="group flex justify-evenly gap-x-4 py-5 my-2 border-2 p-2 rounded-md hover:cursor-pointer" key={location.id}>
+              <div className="flex items-center align-middle w-20 group-hover:opacity-75">
+                <Image
+                  width={256} height={256}
+                  className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                  src={location.imageUrl}
+                  alt=""
+                ></Image>
+              </div>
+              <div className="self-center w-24 align-middle group-hover:opacity-75">
+                  <p className="text-clip self-center text-center text-sm text-gray-900">
+                    {location.name}
+                  </p>
+              </div>
+              <div className="self-center align-middle group-hover:opacity-75">
+                  <p className="text-clip text-center text-sm text-gray-500">
+                    {location.address.length > 25 ? location.address.substring(0,24) + "..." : location.address}
+                  </p>
+              </div>
+              <div className="sm:flex sm:flex-col sm:items-end">
+                <Edit className="invisible group-hover:visible" width={18} height={18}></Edit>
+                <p className="text-sm leading-6 text-gray-900"> Active Deals</p>
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                Added 3h ago
                 </p>
               </div>
-            </div>
-            <div className="sm:flex sm:flex-col sm:items-end">
-              <Edit className="invisible group-hover:visible" width={18} height={18}></Edit>
-              <p className="text-sm leading-6 text-gray-900"> Active Deals</p>
-              <p className="mt-1 text-xs leading-5 text-gray-500">
-               Added 3h ago
-              </p>
-            </div>
-          </li>)
+            </li>)
           })
         }
       </ul>
--- components/business/tab-menu-business.tsx
+++ components/business/tab-menu-business.tsx
@@ -22,7 +22,7 @@ export default function TabMenuBusiness(props:Props) {
         {
           props.session && 
           <div className="border-b border-gray-200 dark:border-gray-700">
-            <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+            <ul className="flex flex-wrap justify-evenly -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
                 <li className="mr-2">
                     <a href="#" onClick={() => setTabName("Locations")} className={tabName === "Locations" ? "inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group" : "inline-flex p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group"}>
                         <Building2 className={tabName === "Locations" ? "w-5 h-5 mr-2 text-blue-600 dark:text-blue-500" : "w-5 h-5 mr-2"}></Building2>Locations
